9b77515a8ab3692c869ab0ab7281054e974f3c48
Stephen Song <stephen.song-1@ou.edu>
2022-02-22 23:24:32 -0600

    Added mybin() function

--- R/mybin.R
+++ R/mybin.R
@@ -1,25 +1,20 @@
 #
-#' mybin()
+#' @title mybin()
 #'
-#' Creates and plots random samples of binary distributions.
+#' @description Creates and plots random sample trials of binary distributions.
 #'
-#' @param iter = iterations, default value 100
-#' @param n = sample size, default value 10
-#' @param p = probability, default value 0.5
+#' @param iter number of iterations, default value 100
+#' @param n sample size, default value 10
+#' @param p probability, default value 0.5
 #'
-#' @return
+#' @return A barplot which tabulates each experiment's results
 #' @export
 #'
-#' @examples
-#' mybin()
-#
-# sampling function
-# iter = iterations, n=sample size
-# set default values
+#' @examples mybin(30,10,0.7)
 mybin=function(iter=100,n=10, p=0.5){
   # make a matrix to hold the samples
   #initially filled with NA's
-  sam.mat=matrix(NA,nr=n,nc=iter, byrow=TRUE)
+  sam.mat=matrix(NA,nrow=n,ncol=iter, byrow=TRUE)
   #Make a vector to hold the number of successes in each trial
   succ=c()
   for( i in 1:iter){
